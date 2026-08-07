6f66e5a9b32bb472289caa8f39f2bc3ab9f2f447
Ethan Hill <ethan.hill1999@outlook.com>
2026-08-07 15:18:51 -0500

    map concept

---
+++ references/Wendy's 01.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Wendy's 01" tilewidth="96" tileheight="32" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="corner01.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="service03.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="service02.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="service01.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="wall03.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="wall02.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="wall01.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="carpet02.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="end-plant01.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="end-base01.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="bench01.png" width="96" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="chair01.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="table01.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="carpet01.png" width="32" height="32"/>
+ </tile>
+</tileset>
